50cce03f30ef48ed9833e4d1703201083904dcef
oscar lopez <oscarlopezg97@gmail.com>
2018-09-03 12:32:03 -0700

    integracion legalPage

--- app/src/main/java/com/cimatronics/subaze/MainActivity.java
+++ app/src/main/java/com/cimatronics/subaze/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cimatronics.subaze;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -88,6 +92,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nosotros) {
 
         } else if (id == R.id.legal) {
+            Intent intent = new Intent(this, legalPage.class);
+            startActivity(intent);
 
         } else if (id == R.id.salir) {
 
---
+++ app/src/main/java/com/cimatronics/subaze/legalPage.java
@@ -0,0 +1,13 @@
+package com.cimatronics.subaze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class legalPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_legal_page);
+    }
+}
