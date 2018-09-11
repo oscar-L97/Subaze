06f885e3a28cf99f254c08818b0f028335f28cdd
oscar lopez <oscarlopezg97@gmail.com>
2018-09-10 19:48:04 -0700

    RecylcerView expandable WIP

---
+++ app/src/main/java/com/cimatronics/subaze/ViewHolders/TitleChildViewHolder.java
@@ -0,0 +1,13 @@
+package com.cimatronics.subaze.ViewHolders;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.bignerdranch.expandablerecyclerview.ViewHolder.ChildViewHolder;
+
+public class TitleChildViewHolder extends ChildViewHolder {
+    public TextView option1, option2;
+    public TitleChildViewHolder(View itemView) {
+        super(itemView);
+    }
+}
--- app/src/main/java/com/cimatronics/subaze/ViewHolders/TitleParentViewHolder.java
+++ app/src/main/java/com/cimatronics/subaze/ViewHolders/TitleParentViewHolder.java
@@ -1,4 +1,19 @@
 package com.cimatronics.subaze.ViewHolders;
 
-public class TitleParentViewHolder {
+import android.media.Image;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.bignerdranch.expandablerecyclerview.ViewHolder.ParentViewHolder;
+import com.cimatronics.subaze.R;
+
+public class TitleParentViewHolder extends ParentViewHolder {
+    public TextView _textView;
+    public ImageButton _imageButton;
+    public TitleParentViewHolder(View itemView) {
+        super(itemView);
+        _textView = (TextView)itemView.findViewById(R.id.parentTitle);
+        _imageButton = (ImageButton) itemView.findViewById(R.id.expandArrow);
+    }
 }
